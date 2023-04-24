ae4517fc1679dddd73d33af25631373f27a4d60e
Sebastian Narvaez <sebasnr95@protonmail.com>
2023-04-24 11:11:01 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+// Una ruta es lo que viene despues del primer slash en una url.
+// Aqui se guardan los componentes que debe cargar Angular por defecto
+// para cada ruta.
+// Se debe agregar la directiva <router-outlet> en app-component.html
+// para que esto funcione.
+const routes: Routes = [
+  {
+    path: '', // Para la ruta vacia, cargar el componente Home
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { TcRatingModule } from '@ngx-tc/rating';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TcRatingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
